54cbd3022161c53def83e1c5f5093beac992a8d0
ilick888 <ilick888@gmail.com>
2020-05-10 00:09:25 +0900

    Create Props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
+const profiles = [
+  {name : "taro", age : 10},
+  {name : "hanako", age : 20},
+  {name : "hana"},
+]
+
 const App = () => {
   return (
-    <h1>
-      <Hello/>
-      <Hello/>
-    </h1>
+    <div>
+      {profiles.map((profile,index) => {
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })}
+    </div>
   );
 }
 
-const Hello = () => {
-  return <div>Hello!</div>
+const User = (props) => {
+return <div>I am {props.name},and {props.age} years old</div>
+}
+
+User.defaultProps = {
+  age:2
 }
 
 export default App;
